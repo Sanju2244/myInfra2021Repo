c4a35668f684e8a0df82700b50d0e6c22bd2f98c
Sanju2244 <103499120+Sanju2244@users.noreply.github.com>
2023-01-19 10:58:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,4 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "my-aws-tf-bucket"
-#     # access_key = "AKIATMWJY32R3RZURDUZ"
-#     # secret_key = "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"
-#     region = "us-east-1"
-#     dynamodb_table = "my-dynamo-db-table"
-#   }
-# }
+
 
 terraform {
   backend "s3" {
